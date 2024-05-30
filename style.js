45c9e3434205a449df13a70746215c9afd0b0c04
updatemee <167770547+updatemee@users.noreply.github.com>
2024-05-30 13:00:32 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+let menuButton = document.getElementById("open-close-icon");
+let mobileMenu = document.getElementById("mobile-menu");
+
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open-menu");
+ 
+});
